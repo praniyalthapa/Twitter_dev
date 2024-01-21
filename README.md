71787c3dec918ed22fb8cd97d5d442d30ac7644b
Praniyal Thapa <praniyalthapa100@gmail.com>
2024-01-21 09:42:41 +0545

    did some basic clearance for project

--- README.md
+++ README.md
@@ -1,22 +1,22 @@
 # Requirements for our Project
-  - In our project user are able to create a post where the post cannot have characters more than 250.
-  - User can upload image also.Our project supports image upload features.
-  
-  - You can only see those post whom you follow and anyone who follows you can comment on your post and like on your post.
-  - You can reply or comment in a comment.
-  - User are able to like a post as well as comment.
-  - User can also retweet the tweet.
+
+- In our project user are able to create a post where the post cannot have characters more than 250.
+- User can upload image also.Our project supports image upload features.
+
+- You can only see those post whom you follow and anyone who follows you can comment on your post and like on your post.
+- You can reply or comment in a comment.
+- User are able to like a post as well as comment on a post.
+- User can also retweet the tweet.
 
 - The following things should be in a User Profile:
- - Name
- - Total Number of Followers (Follower Count)
- - Bio also 
- - Last 10 tweets from the user
+- Name
+- Total Number of Followers (Follower Count)
+- Bio also
+- Last 10 tweets from the user
 
 - Even our project support pagination on tweets
- - We will have our User auth feature 
+- We will have our User auth feature
 
- `Making CRUD is as usual in our project`
- - Every tweets might have a hashtag
+`Making CRUD is as usual in our project`
 
- 
+- Every tweets might have a hashtag
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const express=require('express');
 const connect=require('./config/database');
-// const Tweet=require('./models/tweet');
 const TweetRepository=require('./repository/tweet-repository');
 const Comment=require('./models/comment');
 const app=express();
@@ -8,34 +7,8 @@ app.listen(3000,async()=>{
     console.log('Server Started');
     await connect();
     console.log('MongoDb is connected now');
-    // const tweet=await Tweet.create({
-    //     content:'Third tweet',
+  
 
-    // });
-    // const tweets=await Tweet.find();
-//    tweets.userEmail='nepal123@gmail.com',
-//    tweets.save();  
-    // const tweet=await repo.update('65a235f7c38917bedfcaad37', //it updates document correctly but returns old document not latest document so we used { new: true } in update CRUD
-    // {content:'new latest content here'});
-    // const tweet=await repo.create({content:'my new tweet to add some comments '});
-    // console.log(tweet);
 
-    // tweet.comments.push({content:'This is my first comment'}); //comment also had id here
-   
-    // await tweet.save();
-     const repo=new TweetRepository();
-    // const tweet=await repo.create({content:'Tweet with comment schema as a new change'});
-    // const comment=await Comment.create({content:'new comment'});
-    // tweet.comments.push(comment);
-// await tweet.save();
-const tweet=await repo.getWithComments('65a392f1285bef9ec93fe3e2');
-//implementing offset and limit in our pagination 
-// const tweet=await repo.getAll(0,3);
-//     console.log(tweet[0].contentWithEmail);
-// const tweet=await repo.create({content:'With hooks'});
-console.log(tweet);
-
-//https://mongoosejs.com/docs/api/schematype.html#SchemaType.prototype.index()
-//https://www.mongodb.com/docs/v5.0/indexes/
 
 });
