e668b64ada3046256f69a13df081bf8c99ac94c7
Praniyal Thapa <praniyalthapa100@gmail.com>
2024-01-31 15:48:25 +0545

    added user model

--- src/models/like.js
+++ src/models/like.js
@@ -2,14 +2,21 @@ import mongoose from "mongoose";
 const likeSchema=new mongoose.Schema({
    onModel:{ //on which model we are going to like on comment or a tweet
       type:String,
-      require:true,
+      required:true,
       enum:['Tweet','Commment'] //either we are liking on tweet or comment
    },
    likeable:{ //if onmodel is tweet then likeable is going to be a tweetId 
         type:mongoose.Schema.Types.ObjectId,
-        require:true,
+        required:true,
         refPath:'onModel'
    },
+   user:{
+      type:mongoose.Schema.Types.ObjectId,
+      ref:'User',
+      required:true
+   }
+
+
 },{timestamps:true});
 
 const  Like=mongoose.model('Like',likeSchema);
