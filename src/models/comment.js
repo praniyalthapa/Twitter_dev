f70fc3d5123237867925202b6244a756c14aec82
Praniyal Thapa <praniyalthapa100@gmail.com>
2024-02-11 22:06:24 +0545

    added comment model and did some changes

--- src/models/comment.js
+++ src/models/comment.js
@@ -4,9 +4,21 @@ const commentSchema = new mongoose.Schema({
         type:String,  
         required:true,
     },
-    userEmail:{   //it is not compulsory
-        type:String
+    userId:{   //it is not compulsory
+        type:mongoose.Schema.Types.ObjectId,
+      ref:'User',
+      required:true
     },
+    onModel:{
+        type:String,
+      required:true,
+      enum:['Tweet','Commment'] //we can comment on a tweet or in a comment only
+    },
+  commentable:{
+    type:mongoose.Schema.Types.ObjectId,
+        required:true,
+        refPath:'onModel'
+  }
   
 },{timestamps:true});  //this is for timestamps so old data don't have this feature but new created data has new feature of timestamps
 
