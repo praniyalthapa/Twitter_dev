8763b7152746168f00d4f53f942b63aa4573e0f6
Praniyal Thapa <praniyalthapa100@gmail.com>
2024-02-21 21:30:02 +0545

    added login api and tested login

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import {createTweet, getTweet} from '../../controller/tweet-controller.js';
 import {toogleLike} from '../../controller/like-controller.js';
 import {createComment} from '../../controller/comment-controller.js';
-import {signup} from '../../controller/auth-controller.js';
+import {signup,login} from '../../controller/auth-controller.js';
 const router=express.Router();
 //using this router we can write specific get,post request that is actually going to helps us to hit the particular API routes we want.
 router.post('/tweets',createTweet);
@@ -10,4 +10,5 @@ router.get('/tweets/:id',getTweet);
 router.post('/likes/toogle',toogleLike);
 router.post('/comments',createComment);
 router.post('/signup', signup);
+router.post('/login',login);
 export default router;
